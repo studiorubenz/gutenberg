57bed6bb0ce18301b3e1dfdb99433ec01840f518
Grzegorz Ziolkowski <grzegorz@gziolo.pl>
2020-12-10 08:59:06 +0100

    Try more aggressive approach for feature flag

--- packages/components/src/font-size-picker/next.js
+++ packages/components/src/font-size-picker/next.js
@@ -8,21 +8,20 @@ import { FontSizeControl } from '@wp-g2/components';
  */
 import { __unstableWithNext as withNext } from '../__next/context';
 
-const FontSizePicker = ( props, ref ) => {
-	// @todo Wrapped with div.components-base-control for spacing reasons.
-	// Remove this when the inner Typography Tools component uses VStack
-	// to automatically space elements apart vertically.
-	return (
-		<div className="components-base-control">
-			<FontSizeControl { ...props } ref={ ref } />
-		</div>
-	);
-};
+const FontSizePicker =
+	process.env.COMPONENT_SYSTEM_PHASE === 1
+		? ( props, ref ) => {
+				// @todo Wrapped with div.components-base-control for spacing reasons.
+				// Remove this when the inner Typography Tools component uses VStack
+				// to automatically space elements apart vertically.
+				return (
+					<div className="components-base-control">
+						<FontSizeControl { ...props } ref={ ref } />
+					</div>
+				);
+		  }
+		: undefined;
 
 export function withNextComponent( current ) {
-	return withNext(
-		current,
-		process.env.COMPONENT_SYSTEM_PHASE === 1 ? FontSizePicker : undefined,
-		'WPComponentsFontSizePicker'
-	);
+	return withNext( current, FontSizePicker, 'WPComponentsFontSizePicker' );
 }
