9d85e24862d76a7489bd0f28548ab218af4e11d4
Grzegorz Ziolkowski <grzegorz@gziolo.pl>
2020-12-10 09:39:58 +0100

    Enable COMPONENT_SYSTEM_PHASE flag again

--- webpack.config.js
+++ webpack.config.js
@@ -102,7 +102,7 @@ module.exports = {
 				parseInt(
 					process.env.npm_package_config_COMPONENT_SYSTEM_PHASE,
 					10
-				) || 0
+				) || 1
 			),
 			'process.env.FORCE_REDUCED_MOTION': JSON.stringify(
 				process.env.FORCE_REDUCED_MOTION
